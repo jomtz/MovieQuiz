193833f3ee770b23f36c6e631228fba1341171b9
Jos Martinez <46319076+josmartinez@users.noreply.github.com>
2021-02-23 10:32:14 -0500

    Update Questions

--- app/src/main/java/com/josmartinez/androidquiz/QuizViewModel.kt
+++ app/src/main/java/com/josmartinez/androidquiz/QuizViewModel.kt
@@ -1,7 +1,6 @@
 package com.josmartinez.androidquiz
 
 import androidx.lifecycle.ViewModel
-import kotlin.random.Random
 
 private const val TAG = "QuizViewModel"
 
@@ -10,12 +9,20 @@ class QuizViewModel : ViewModel() {
     var currentIndex = 0
 
     private val questionBank = listOf(
-        Question(R.string.question_kotlin, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
+        Question(R.string.question_number_one, false),
+        Question(R.string.question_number_two, true),
+        Question(R.string.question_number_three, true),
+        Question(R.string.question_number_four, false),
+        Question(R.string.question_number_five, false),
+        Question(R.string.question_number_six, false),
+        Question(R.string.question_number_seven, true),
+        Question(R.string.question_number_eight, false),
+        Question(R.string.question_number_nine, false),
+        Question(R.string.question_number_ten, true),
+        Question(R.string.question_number_eleven, false),
+        Question(R.string.question_number_twelve, true),
+        Question(R.string.question_number_thirteen, true),
+        Question(R.string.question_number_fourteen, true))
 
     val currentQuestionAnswer: Boolean get() = questionBank[currentIndex].answer
 
